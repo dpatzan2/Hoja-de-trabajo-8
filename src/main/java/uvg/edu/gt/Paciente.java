88453dd5c77d55ce4bbbb2f559d9ed0589a56cd4
Diego Patzan <diegopatzan24@gmail.com>
2024-04-07 21:04:28 -0600

    pacient model

--- src/main/java/uvg/edu/gt/Paciente.java
+++ src/main/java/uvg/edu/gt/Paciente.java
@@ -1,4 +1,35 @@
 package uvg.edu.gt;
 
-public class Paciente {
+public class Paciente implements Comparable<Paciente> {
+    private String nombre;
+    private String sintoma;
+    private char codigoEmergencia;
+
+    public Paciente(String nombre, String sintoma, char codigoEmergencia) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigoEmergencia = codigoEmergencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSintoma() {
+        return sintoma;
+    }
+
+    public char getCodigoEmergencia() {
+        return codigoEmergencia;
+    }
+
+    @Override
+    public int compareTo(Paciente otro) {
+        return Character.compare(this.codigoEmergencia, otro.getCodigoEmergencia());
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + sintoma + ", " + codigoEmergencia;
+    }
 }
